8d173453dc08e7327a7e9158c6ce8d64291433ae
proc0 <alanvx@gmail.com>
2025-11-11 14:25:45 -0800

    updating cmakelists with raystart updates

--- include/config.h
+++ include/config.h
@@ -3,5 +3,5 @@
 #define PROJECT_NAME "Maxwells-Demon"
 #define VERSION "0.1.0"
 #define DEBUG 1
-#define DATE_BUILD "2025-10-11 22:17:07"
+#define DATE_BUILD "2025-11-11 14:24:02"
 #define DIR_ASSETS "assets"

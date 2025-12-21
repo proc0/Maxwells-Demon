ffa15b2a73da0dea7eadb20d4eb206e162d54f28
proc0 <alanvx@gmail.com>
2025-12-21 13:55:46 -0800

    adding cmakelists command to generate LSP info

--- include/config.h
+++ include/config.h
@@ -3,5 +3,5 @@
 #define PROJECT_NAME "Maxwells-Demon"
 #define VERSION "0.1.0"
 #define DEBUG 1
-#define DATE_BUILD "2025-12-14 06:04:53"
+#define DATE_BUILD "2025-12-21 13:43:16"
 #define DIR_ASSETS "assets"

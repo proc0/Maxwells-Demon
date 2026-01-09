e663b8d1ca1ec81af82130ec80341745ca84b66e
proc0 <alanvx@gmail.com>
2026-01-09 07:30:55 -0800

    reducing max density to avoid stack overflow

--- include/config.h
+++ include/config.h
@@ -3,5 +3,5 @@
 #define PROJECT_NAME "Maxwells-Demon"
 #define VERSION "0.1.0"
 #define DEBUG 1
-#define DATE_BUILD "2026-01-09 07:14:10"
+#define DATE_BUILD "2026-01-09 07:29:08"
 #define DIR_ASSETS "assets"

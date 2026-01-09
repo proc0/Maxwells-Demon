6700847e32d9c51aaa9e307da06ddebdb76f67eb
proc0 <alanvx@gmail.com>
2026-01-09 08:31:03 -0800

    changing dist main to index

--- include/config.h
+++ include/config.h
@@ -2,6 +2,6 @@
 
 #define PROJECT_NAME "Maxwells-Demon"
 #define VERSION "0.1.0"
-#define DEBUG 0
-#define DATE_BUILD "2026-01-09 07:49:54"
+#define DEBUG 1
+#define DATE_BUILD "2026-01-09 07:53:02"
 #define DIR_ASSETS "assets"
